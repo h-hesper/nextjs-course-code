d97737f7a2d769d243408665a1631534e46974cb
h-hesper <102551979+h-hesper@users.noreply.github.com>
2022-04-04 16:34:32 -0400

    setup

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
-const HomePage(){
-  return()
+import MeetupList from "../components/meetups/MeetupList";
+
+const DummyMeetups = [
+  {id:"m1",
+title:"A First Meetup",
+image:"https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+address:"5, 12345 Cavendish blvd",
+description:"this is our first meetup!"},
+{id:"m2",
+title:"A Second Meetup",
+image:"https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+address:"5, 12345 Cavendish blvd",
+description:"this is our second meetup!"}
+] 
+
+
+const HomePage = () => {
+  return(
+    <MeetupList meetups={DummyMeetups}/>
+  )
 }
 
 export default HomePage;
