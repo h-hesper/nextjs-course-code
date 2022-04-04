d58395623979652a9be8f92927f5198db51ae965
h-hesper <102551979+h-hesper@users.noreply.github.com>
2022-04-04 17:32:46 -0400

    adding layout

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,16 @@
+import NewMeetupForm from "../../components/meetups/NewMeetupForm"
+
+
+const NewMeetupPage = () =>{
+
+    const addMeetupHandler=(meetupData)=>{
+
+        console.log(meetupData)
+
+    }
+
+return <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+
+}
+
+export default NewMeetupPage
